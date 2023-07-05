757a2cbdff8e122c2dfb02f0e7a42614665e43bd
andreifloreatzi <andrei_fl97@yahoo.com>
2023-07-05 04:55:49 +0300

    Generate_Excel_files.py added + excel files

---
+++ scripts/filler_sentences/generate_excel_files.py
@@ -0,0 +1,196 @@
+import json, os
+import rowordnet as rwn
+import xlsxwriter
+import time
+
+_LEGEND_ = "       Introduceți numele dumneavoastră, apoi completați toate celulele albastre cu câte o propoziție care sa conțină cuvântul din celula roșie, între acolade { } și cu sensul definit de celula portocalie."
+_MAXIMUM_FILE_COUNT_ = 100
+_MINIMUM_SENTENCES_PER_FILE_ = 100
+_MAX_LITERALS_ = 5000
+_INPUT_PATH_ = "scripts/filler_sentences/input/"
+_OUTPUT_PATH_ = "scripts/filler_sentences/output/"
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+def generate_excel_file(data, name, literals_synsets) -> None:
+
+    count_total_sentences_to_fill = 0
+    starting_space = 2
+
+    # Cream xlsx-ul
+    workbook = xlsxwriter.Workbook( _OUTPUT_PATH_ + name )
+    worksheet = workbook.add_worksheet()
+    vcenter = workbook.add_format()
+    vcenter.set_align('vcenter')
+    worksheet.set_column('A:XFD', None, vcenter)
+
+
+    # Widen first and second column and center text
+    vcenter.set_align('vcenter')
+    worksheet.set_column('A:XFD', None, vcenter)
+    worksheet.set_column("A:A", 25 , vcenter)
+    worksheet.set_column("B:B", 250, vcenter)
+    worksheet.set_default_row(26)
+    worksheet.set_row(0,50)
+
+    # Colorarea
+    sentence_already_there = []
+    sentence_color_to_fill = []
+    count_sentences_format = workbook.add_format({ 'font_color':'gray'})
+    synset_format = workbook.add_format({ 'font_color':'gray'})
+    name_color = workbook.add_format({'bold':True, 'font_color':'black', "bg_color":"#ffc2c2", "align":"vcenter"})
+    name_to_fill_color = workbook.add_format({'bold':True, 'font_color':'black', "bg_color":"#ffd9d9", "align":"vcenter"})
+    divider_color = workbook.add_format({'bold':True, 'bg_color':'#808080', "align":"vcenter"})
+    definition_color = workbook.add_format({'bold':True, 'font_color':'black', "bg_color":"#FFB266", "align":"vcenter"})
+    sentence_color_to_fill.append(workbook.add_format({'bold':True, "bg_color":"A9E3FF", "align":"vcenter"}))
+    sentence_color_to_fill.append(workbook.add_format({'bold':True, "bg_color":"90DBFF", "align":"vcenter"}))
+    sentence_already_there.append(workbook.add_format({'bold':True, 'bg_color':'#AFFFAF', "align":"vcenter"}))
+    sentence_already_there.append(workbook.add_format({'bold':True, 'bg_color':'#9BFF9B', "align":"vcenter"}))
+    literal_color = workbook.add_format({'bold':True, 'bg_color':'#ff5252', "align":"vcenter"})
+
+    # Adaugăm primul divider
+    worksheet.write(1, 0, "", divider_color)
+    worksheet.write(1, 1, "", divider_color)
+
+    # Primul rând din excel
+    worksheet.write(starting_space, 0, "Numele și Prenumele:", name_color)
+    worksheet.write(starting_space, 1, "  <introduceți numele aici>", name_to_fill_color)
+
+    # Pentru fiecare pereche (literal, synset) umplem fisierul excel cu blocuri de forma:
+    '''
+    LITERAL  |   LITERALI:GLOSA                              <- Rândul 12*(al câtelea bloc) + starting_space + 2
+    _________|__________________________________________
+    SYNSET   |   PROPOZITII EXISTENTE
+    _________|__________________________________________
+             |   SPATIU DE UMPLUT CU PROPOZITII
+    _________| _________________________________________
+    '''
+    for index in range(0,len(literals_synsets)):
+        literal = literals_synsets[index][0]
+        synset = literals_synsets[index][1]
+
+        # Filling blank sentences with color
+        glosa_start_row = 2 + 12*index + starting_space
+        for i in range(glosa_start_row,glosa_start_row + 11):
+            worksheet.write(i , 1, "", sentence_color_to_fill[i%2])
+
+        # Legenda
+        worksheet.write(0 , 1, _LEGEND_, name_color)
+
+        # Divider
+        worksheet.write(glosa_start_row-1, 0, "", divider_color)
+        worksheet.write(glosa_start_row-1, 1, "", divider_color)
+
+        # Pentru fiecare synset_id introducem literal, glosa, synset si propozitiile pe pozițiile corespunzătoare
+        # Synset_id este necesar pentru citirea de date
+        literals_glosa = str(wn(synset).literals) + "  " + str(wn(synset).definition)
+        worksheet.write(glosa_start_row, 0, literal, literal_color)
+        worksheet.write(glosa_start_row + 1, 0, synset, synset_format)
+        worksheet.write(glosa_start_row , 1, literals_glosa, definition_color)
+
+        # Randul de pe care incepem sa scriem propozitiile
+        literal_start_row = 3 + 12*index + starting_space
+
+        sentence_count = len(data[literal]["synsets"][synset])
+
+        # data[literal]["synsets"][syn_id] --- Ar trebui sa aiba cel mult 9 propozitii
+        '''
+        if sentence_count > 9:
+                print("Error: 10 or more sentences for {} -- {} | synset -- literal".format(synset, literal))
+        '''
+
+        # Introducem propozitiile deja existente:
+        for sen_index in range(sentence_count):
+            sentence = data[literal]["synsets"][synset][sen_index]
+            sen_row = literal_start_row + sen_index
+            worksheet.write(sen_row , 1, sentence, sentence_already_there[sen_row%2])
+
+        count_total_sentences_to_fill += 10 - sentence_count
+
+    # In colțul stănga sus al fisierului scriem câte propoziții sunt de umplut, pentru a ține evidența voluntarilor
+    worksheet.write(0 , 0, count_total_sentences_to_fill, count_sentences_format)
+    workbook.close()
+
+
+
+
+if __name__ == "__main__":
+
+    # Loading data
+    wn = rwn.RoWordNet()
+    if not os.path.exists(_OUTPUT_PATH_):
+        os.makedirs(_OUTPUT_PATH_)
+    path_to_json = os.path.join(_INPUT_PATH_, "literal2synset_data.json")
+
+    with open(path_to_json, encoding="utf8") as f:
+        data = json.load(f)
+
+    '''
+    JSON data format:
+    {
+    "estimație":{
+        "score":3.555555555555556,
+        "synsets":{
+            "ENG30-05736149-n":[
+            ],
+            "ENG30-00875246-n":[
+            ],
+            "ENG30-05803379-n":[
+                "În mai 2008, The Boston Globe a stabilit o {estimație} de 670.000 de persoane pentru acest grup.",
+                "În 1925 a pus bazele teoriei {estimației}, care face parte din domeniul statisticii matematice.",
+                "Folosind metoda variabilei cefeide, în 2004 a fost obținută o {estimație} de 2,51 ± 0,13 milioane de ani lumină.",
+                "Deoarece ieșirile sunt aleatoare, ele pot fi considerate doar ca {estimații} ale caracteristicilor adevărate ale sistemului."
+            ]
+        }
+    }
+    "armată": { ... }
+    ...
+    }
+    '''
+
+    # Sortare dictionar
+    sorted_data = {k: v for k, v in sorted(data.items(), key=lambda item: item[1]['score'],reverse=True)}
+
+    literal_cnt = _MAX_LITERALS_
+    file_cnt = 1
+    literals = []
+    total_sen_cnt = 0
+    literals_synsets_list = []
+
+    for literal in sorted_data:
+
+        literal_cnt -= 1
+        if file_cnt > _MAXIMUM_FILE_COUNT_:
+            break
+        if literal_cnt == 0:
+            break
+
+        # Ne interesează literali cu cel puțin 2 synseturi
+        if len(sorted_data[literal]["synsets"]) > 1:
+            for synset in sorted_data[literal]["synsets"]:
+
+                # Generăm fișier când găsim _MINIMUM_SENTENCES_PER_FILE_ de propoziții "lipsă"
+                if total_sen_cnt >= _MINIMUM_SENTENCES_PER_FILE_:
+                    generate_excel_file(sorted_data, name=f"wsd_{file_cnt}.xlsx", literals_synsets=literals_synsets_list)
+                    file_cnt += 1
+                    total_sen_cnt = 0
+                    literals_synsets_list = []
+                    printProgressBar(_MAX_LITERALS_ - literal_cnt, _MAX_LITERALS_, prefix = 'Progress:', suffix = 'Complete', length = 50)
+                    #printProgressBar(file_cnt, _MAXIMUM_FILE_COUNT_, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+                count_sentences = len(sorted_data[literal]["synsets"][synset])
+
+                #Introducem doar 2-upluri cu mai putin de 10 propozitii per synset:
+                if count_sentences < 10:
+                    literals_synsets_list.append((literal, synset))
+                    total_sen_cnt += 10 - count_sentences
+
+
--- scripts/filler_sentences/read_excel_files.py
+++ scripts/filler_sentences/read_excel_files.py
@@ -1,27 +1,6 @@
 import json, os
 
-def generate_excel_file() -> None:
-    # scrie fisierul in folder output
+# Adaugam datele din excel in database.
 
-
-if __name__ == "__main":
-    os.makedirs("output", exists_ok=True)
-
-    # incarci json
-
+#def read_excel():
     
-
-    # sortare dict
-    sorted_dict = {k: v for k, v in sorted(x.items(), key=lambda item: item[1]['score'])}
-
-    literal_cnt = 5000
-    file_cnt = 0
-    literals = []
-    for key, value in sorted_dict:
-        literal_cnt -= 1
-        if literal_cnt == 0:
-            break
-        # adaugi literali ( numarand propozitiile lipsa )
-        # cand prop lipsa > 100, scrii fisierul cu un nume unic
-        if scrie_fisier:
-            generate_excel_file(name=f"wsd_{file_cnt}.xlsx", literals=)
